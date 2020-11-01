5d8c33a1b54636aca588ac4a5ba01611be58a8e7
Ramya.U <ramyayadav2016@gmail.com>
2020-11-01 19:27:09 +0530

    [Ramya] Add . Added UC_2

---
+++ MoodAnalyserApp/MoodAnalyser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserApp
+{
+    public class MoodAnalyser
+    {
+        public string message;
+        public MoodAnalyser() 
+        {
+        
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MoodAnalyser"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        /// <summary>
+        /// Analyses the mood and returns happy or sad.
+        /// </summary>
+        /// <returns></returns>
+        public string AnalyseMood()
+        {
+            string mood;
+            try
+            {
+                mood = this.message.Contains("Sad") || this.message.Contains("sad") ? "Sad" : "Happy";
+            }
+            catch
+            {
+                mood = "Happy";
+            }
+            return mood;
+        }
+    }
+}
